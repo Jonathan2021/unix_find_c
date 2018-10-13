642641b20d2dd73e01b926923ec949ce4e21bc71
Jonathan Sands <sands.kids@free.fr>
2018-10-13 18:36:43 +0200

    Basic tests.

--- tests/testsimple.sh
+++ tests/testsimple.sh
@@ -1,9 +1,31 @@
 #!/bin/sh
 
+# Check that output is equivalent to that of the real find.
+
 T1=$(mktemp)
 T2=$(mktemp)
 
-$FIND files | sort > $T1
-find files | sort > $T2
+while read args ; do
+  $FIND $args | sort > $T1
+  find $args | sort > $T2
 
-diff -q $T1 $T2 && rm $T1 $T2
+  diff $T1 $T2 || exit 1
+done <<EOF
+files
+files -type d
+files -name a
+files -print
+files -exec echo hello ;
+files -execdir echo bye ;
+files -perm 755
+files -perm +755
+files -perm -5
+files -user jonathan
+files -user duncan
+files -group jonathan
+files -group duncan
+files -newer ./testsimple.sh
+files -true
+EOF
+
+rm $T1 $T2
