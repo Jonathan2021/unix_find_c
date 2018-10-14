63205b5314a474504bfcae3c3b5952a2629d6672
Jonathan Sands <sands.kids@free.fr>
2018-10-14 09:20:11 +0200

    Change colours back after printing.

--- runtests.sh
+++ runtests.sh
@@ -4,14 +4,15 @@ export FIND=../myfind
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
+END='\e[0m'
 
 cd tests
 
 for TEST in *.sh ; do
   if ./$TEST ; then
-    echo "$GREEN$TEST passed"
+    echo "$GREEN$TEST passed$END"
   else
-    echo "$RED$TEST failed"
+    echo "$RED$TEST failed$END"
     exit 1
   fi
 done
